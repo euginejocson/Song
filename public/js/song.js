3768f4d43fcdfa0cc5fe48a200cf8785c82a9aaa
euginejocson <euginejocson@gmail.com>
2021-02-17 00:14:12 +0800

    remove app.js

--- public/js/song.js
+++ public/js/song.js
@@ -1,13 +1,13 @@
 $(document).ready( function () {
 	// $('#table-song').DataTable();
-	$('#table-song').DataTable({
+	$('#table-song').dataTable({
 		processing: true,
 		serverSide: true,
 		ajax: '/song/list',
 		columns: [
 			{ data: 'id', name: 'id' },
 			{ data: 'title', name: 'title' },
-			{ data: 'author', name: 'author' },
+			{ data: 'artist', name: 'artist' },
 			{ data: 'created_at', name: 'created_at' }
 		]
 	});
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -93,10 +93,14 @@
 
 	<!-- Scripts -->
 	 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
-	 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+	 <script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
  	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
- 	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
-	<script src="{{ asset('js/app.js') }}" defer></script>
+	 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
+
+	{{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 	<script src="js/scripts.js"></script>
 	@yield('script')
 </body>
